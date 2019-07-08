f42043a8807560446ad31e89e11bdcbf12dd6599
Andrés <ander74@gmail.com>
2019-07-08 12:21:54 +0200

    Splash Actualizada en iOS

--- Quattro.iOS/AppDelegate.cs
+++ Quattro.iOS/AppDelegate.cs
@@ -1,9 +1,29 @@
 ﻿using Foundation;
 using MvvmCross.Platforms.Ios.Core;
+using UIKit;
 
 namespace Quattro.iOS {
+
     [Register("AppDelegate")]
     public class AppDelegate : MvxApplicationDelegate {
+
+
+        public override bool FinishedLaunching(UIApplication application, NSDictionary launchOptions) {
+
+            var result = base.FinishedLaunching(application, launchOptions);
+
+            CustomizarApariencia();
+
+            return result;
+
+        }
+
+
+        private void CustomizarApariencia() {
+
+            // Aquí podemos cambiar la apariencia de la aplicación.
+
+        }
 
     }
 }
