64cba724d555d97b112287bd24cd8a1ce6ccff1d
Andrés <ander74@gmail.com>
2019-07-10 08:31:05 +0200

    ListaCalendario Added

--- Quattro.iOS/Views/CalendarioView.cs
+++ Quattro.iOS/Views/CalendarioView.cs
@@ -1,15 +1,32 @@
 ﻿using System;
-
+using MvvmCross.Binding.BindingContext;
+using MvvmCross.Platforms.Ios.Binding.Views;
+using MvvmCross.Platforms.Ios.Views;
+using Quattro.Core.ViewModels;
 using UIKit;
 
 namespace Quattro.iOS.Views {
-    public partial class CalendarioView : UIViewController {
+
+    public partial class CalendarioView : MvxViewController<CalendarioViewModel> {
+
         public CalendarioView() : base("CalendarioView", null) {
         }
 
         public override void ViewDidLoad() {
             base.ViewDidLoad();
             // Perform any additional setup after loading the view, typically from a nib.
+
+            var source = new MvxSimpleTableViewSource(ListaCalendario, "CalendarioCell", CalendarioCell.Key);
+            ListaCalendario.RowHeight = 75;
+            var set = this.CreateBindingSet<CalendarioView, CalendarioViewModel>();
+            set.Bind(source).To(vm => vm.ListaDias);
+            set.Bind(BtRetroceder).To(vm => vm.AnteriorPulsadoCommand);
+            set.Bind(BtSiguiente).To(vm => vm.SiguientePulsadoCommand);
+            set.Apply();
+
+            ListaCalendario.Source = source;
+            ListaCalendario.ReloadData();
+            
         }
 
         public override void DidReceiveMemoryWarning() {
--- Quattro.iOS/Views/CalendarioView.designer.cs
+++ Quattro.iOS/Views/CalendarioView.designer.cs
@@ -14,8 +14,61 @@ namespace Quattro.iOS.Views
     [Register ("CalendarioView")]
     partial class CalendarioView
     {
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton BtRetroceder { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton BtSiguiente { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UILabel LabelAcumuladas { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UILabel LabelNocturnas { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UITableView ListaCalendario { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIView ViewNavegacion { get; set; }
+
         void ReleaseDesignerOutlets ()
         {
+            if (BtRetroceder != null) {
+                BtRetroceder.Dispose ();
+                BtRetroceder = null;
+            }
+
+            if (BtSiguiente != null) {
+                BtSiguiente.Dispose ();
+                BtSiguiente = null;
+            }
+
+            if (LabelAcumuladas != null) {
+                LabelAcumuladas.Dispose ();
+                LabelAcumuladas = null;
+            }
+
+            if (LabelNocturnas != null) {
+                LabelNocturnas.Dispose ();
+                LabelNocturnas = null;
+            }
+
+            if (ListaCalendario != null) {
+                ListaCalendario.Dispose ();
+                ListaCalendario = null;
+            }
+
+            if (ViewNavegacion != null) {
+                ViewNavegacion.Dispose ();
+                ViewNavegacion = null;
+            }
         }
     }
 }
